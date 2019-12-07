c96cc7d252114e50d1679d3a319df447a88a9fbb
Bob Kuczewski <bobkuczewski@salk.edu>
2019-12-06 23:06:15 -0800

    Added code from concentriccircles.py with wheelEvent handling.
    This version no longer draws the image and instead draws concentric
    circles "zoomed" by the mouse wheel.

--- source/PySide2/py_swift_gui.py
+++ source/PySide2/py_swift_gui.py
@@ -6,6 +6,68 @@ from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QAction
 from PySide2.QtGui import QPixmap
 from PySide2.QtCore import Slot, qApp
 
+from PySide2.QtCore import QRect, QRectF, QSize, Qt, QTimer
+from PySide2.QtGui import QColor, QPainter, QPalette, QPen
+from PySide2.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+        QSizePolicy, QWidget)
+
+
+class ZoomPanWidget(QWidget):
+    def __init__(self, parent=None):
+        super(ZoomPanWidget, self).__init__(parent)
+
+        self.floatBased = False
+        self.antialiased = False
+        self.frameNo = 0
+
+        self.setBackgroundRole(QPalette.Base)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+    def setFloatBased(self, floatBased):
+        self.floatBased = floatBased
+        self.update()
+
+    def setAntialiased(self, antialiased):
+        self.antialiased = antialiased
+        self.update()
+
+    def minimumSizeHint(self):
+        return QSize(50, 50)
+
+    def sizeHint(self):
+        return QSize(180, 180)
+
+    def nextAnimationFrame(self):
+        self.frameNo += 1
+        self.update()
+
+    def wheelEvent(self, event):
+        print ( "Wheel Event with delta() = " + str(event.delta()) )
+        self.frameNo += event.delta()/12
+        if self.frameNo < 0:
+          self.frameNo = 0
+        self.update()
+
+        #__import__('code').interact(local={k: v for ns in (globals(), locals()) for k, v in ns.items()})
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing, self.antialiased)
+        painter.translate(self.width() / 2, self.height() / 2)
+
+        for diameter in range(0, 256, 9):
+            delta = abs((self.frameNo % 128) - diameter / 2)
+            alpha = 255 - (delta * delta) / 4 - diameter
+            if alpha > 0:
+                painter.setPen(QPen(QColor(0, diameter / 2, 127, alpha), 3))
+
+                if self.floatBased:
+                    painter.drawEllipse(QRectF(-diameter / 2.0,
+                            -diameter / 2.0, diameter, diameter))
+                else:
+                    painter.drawEllipse(QRect(-diameter / 2,
+                            -diameter / 2, diameter, diameter))
+
 
 class MainWindow(QMainWindow):
     def __init__(self, widget, fname):
@@ -58,7 +120,9 @@ if __name__ == "__main__":
     pixmap = QPixmap(fname)
     label.setPixmap(pixmap)
 
-    window = MainWindow(label,fname)
+    zpa = ZoomPanWidget()
+
+    window = MainWindow(zpa,fname)
     # window.resize(pixmap.width(),pixmap.height())  # Optionally resize to image
 
     window.show()
