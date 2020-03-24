4e30997c00a7d279bf4c3b80bc256cbdf015ab0a
Bob Kuczewski <bobkuczewski@salk.edu>
2020-03-23 19:24:32 -0700

    Moved progress bar code up near generate_scales (first to get progress).

--- source/PySide2/alignem_swift.py
+++ source/PySide2/alignem_swift.py
@@ -104,6 +104,63 @@ def link_stack():
     #__import__('code').interact(local={k: v for ns in (globals(), locals()) for k, v in ns.items()})
 
 
+
+
+class RunProgressDialog(QDialog):
+    """
+    Simple dialog that consists of a Progress Bar and a Button.
+    Clicking on the button results in the start of a timer and
+    updates the progress bar.
+    """
+    def __init__(self):
+        super().__init__()
+        print ( "RunProgressDialog constructor called" )
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Progress Bar')
+        self.progress = QProgressBar(self)
+        self.progress.setGeometry(0, 0, 300, 25)
+        self.progress.setMaximum(100)
+        #self.button = QPushButton('Start', self)
+        #self.button.move(0, 30)
+        self.setModal(True)
+        self.show()
+        self.onButtonClick()
+
+        #self.button.clicked.connect(self.onButtonClick)
+
+    def onButtonClick(self):
+        self.calc = RunnableThread()
+        self.calc.countChanged.connect(self.onCountChanged)
+        self.calc.start()
+
+    def onCountChanged(self, value):
+        self.progress.setValue(value)
+
+COUNT_LIMIT = 100
+class RunnableThread(QThread):
+    """
+    Runs a counter thread.
+    """
+    countChanged = Signal(int)
+
+    def run(self):
+        count = 0
+        while count < COUNT_LIMIT:
+            count +=1
+            time.sleep(0.05)
+            self.countChanged.emit(count)
+
+window = None
+def run_progress():
+    global window
+    print ( "Run started" )
+    window = RunProgressDialog()
+
+
+
+
 def generate_scales ():
     print ( "generate_scales inside alignem_swift called" )
     main_win.status.showMessage("Generating Scales ...")
@@ -466,59 +523,6 @@ def clear_match_points():
                 layer['images'][role]['metadata']['match_points'] = []
                 layer['images'][role]['metadata']['annotations'] = []
         main_win.update_panels()
-
-
-class RunProgressDialog(QDialog):
-    """
-    Simple dialog that consists of a Progress Bar and a Button.
-    Clicking on the button results in the start of a timer and
-    updates the progress bar.
-    """
-    def __init__(self):
-        super().__init__()
-        print ( "RunProgressDialog constructor called" )
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Progress Bar')
-        self.progress = QProgressBar(self)
-        self.progress.setGeometry(0, 0, 300, 25)
-        self.progress.setMaximum(100)
-        #self.button = QPushButton('Start', self)
-        #self.button.move(0, 30)
-        self.setModal(True)
-        self.show()
-        self.onButtonClick()
-
-        #self.button.clicked.connect(self.onButtonClick)
-
-    def onButtonClick(self):
-        self.calc = RunnableThread()
-        self.calc.countChanged.connect(self.onCountChanged)
-        self.calc.start()
-
-    def onCountChanged(self, value):
-        self.progress.setValue(value)
-
-COUNT_LIMIT = 100
-class RunnableThread(QThread):
-    """
-    Runs a counter thread.
-    """
-    countChanged = Signal(int)
-
-    def run(self):
-        count = 0
-        while count < COUNT_LIMIT:
-            count +=1
-            time.sleep(0.05)
-            self.countChanged.emit(count)
-
-window = None
-def run_progress():
-    global window
-    print ( "Run started" )
-    window = RunProgressDialog()
 
 
 link_stack_cb = CallbackButton('Link Stack', link_stack)
