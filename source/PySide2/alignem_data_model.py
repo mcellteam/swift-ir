ceda1bdd4f6d24f9270617a802213f337d4b2063
Bob Kuczewski <bobkuczewski@salk.edu>
2020-04-02 19:27:22 -0700

    Updated to put the alignment_option at the scale level (0.26 -> 0.27)
    This version modifies the data model to put the alignment_option at the
    scale level. It had been at each layer of the stack.

--- source/PySide2/alignem_data_model.py
+++ source/PySide2/alignem_data_model.py
@@ -4,10 +4,56 @@ AlignEm is intended to provide a tool for supporting image alignment
 using any number of technologies.
 """
 
+def upgrade_data_model(data_model):
+  # Upgrade the "Data Model"
+  if data_model['version'] != new_project_template['version']:
+    # Begin the upgrade process:
+    if data_model ['version'] <= 0.27:
+      # Need to modify the data model from 0.26 or lower up to 0.27
+      # The "alignment_option" had been in the method_data at each scale
+      # This new version defines it only at the scale level
+      # So loop through each scale and move the alignment_option from the method_data to the scale
+      for scale_key in data_model['data']['scales'].keys():
+        scale = data_model['data']['scales'][scale_key]
+        stack = scale['alignment_stack']
+        current_alignment_options = []
+        for layer in stack:
+          if "align_to_ref_method" in layer:
+            align_method = layer['align_to_ref_method']
+            if 'method_data' in align_method:
+              if 'alignment_option' in align_method['method_data']:
+                current_alignment_options.append(align_method['method_data']['alignment_option'])
+        # The current_alignment_options list now holds all of the options for this scale (if any)
+        # Start by setting the default for the scale option to "init_affine" if none are found
+        scale_option = "init_affine"
+        # If any options were found in this scale, search through them for most common
+        if len(current_alignment_options) > 0:
+          # They should all be the same at this scale, so set to the first
+          scale_option = current_alignment_options[0]
+          # But check if any are different and then find the most common
+          if current_alignment_options.count(current_alignment_options[0]) != len(current_alignment_options):
+            # There are some that are different, so find the most common option
+            scale_option = max ( set(current_alignment_options), key=current_alignment_options.count )
+        # At this point "scale_option" should be the one to use
+        if not ('method_data' in scale):
+          # Ensure that there's some method data
+          scale['method_data'] = {}
+        # Finally set the value
+        scale['method_data']["alignment_option"] = scale_option
+
+      # Now the data model is at 0.27, so give it the appropriate version
+      data_model ['version'] = 0.27
+
+    # Make the final test
+    if data_model ['version'] != new_project_template['version']:
+      # The data model could not be upgraded, so return a string with the error
+      data_model = 'Version mismatch. Expected "' + str(new_project_template['version']) + '" but found ' + str(data_model['version'])
+  return data_model
+
 
 new_project_template = \
 {
-  "version": 0.26,
+  "version": 0.27,
   "method": "None",
   "user_settings": {
     "max_image_file_size": 100000000
@@ -24,6 +70,9 @@ new_project_template = \
     ],
     "scales": {
       "scale_1": {
+        "method_data": {
+          "alignment_option": "init_affine"
+        },
         "null_cafm_bias": False,
         "use_bounding_rect": False,
         "alignment_stack": []
