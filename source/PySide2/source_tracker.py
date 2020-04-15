3fd7f962a97b164109d1e9a28fa1fd8852e8b47e
Bob Kuczewski <bobkuczewski@salk.edu>
2020-04-15 03:38:31 -0700

    Added support for detecting/removing GIT Merge Markers from source_info.json
    When GIT detects a change to source_info.json, it will add merge
    markers indicating the change:
    
      {
      <<<<<<< HEAD
        "current_hash": "052f4245058a8f4f84886d27cb1c096f7ca2e483",
      =======
        "current_hash": "03f477aebda2d6656ae6ea911fa5ee98278fdd72",
      >>>>>>> 7b35d2666beda47d7d1024a181fb2e005c84ea2a
        "tagged_versions": {
          "150e0acd86a3c57a7d79fde4b3c4e225b55f26e4": "0.2.3",
          "2eac19472e2631173bbbe09d3c4251e219508602": "0.2.2",
          "5d9fc94902a283bf458d7be61f6dccaafd5fb553": "0.2.1"
        }
      }
    
    This makes the entire file non-legal-JSON and it cannot be loaded
    with the JSON library. The previous solution required manually
    editing the file to remove the markers and all text in between.
    Then running alignem again would add the "current_hash" value
    for the current source code.
    
    Since this manual editing would need to be done for every merge,
    this commit adds a small amount of code to source_tracker.py to
    do that automatically. Since source_tracker.py is already reading
    and updating the file, it is the logical place to detect merge
    markers and remove them.
    
    Of course, any other merge problems will need to be fixed with
    the normal process.

--- source/PySide2/source_tracker.py
+++ source/PySide2/source_tracker.py
@@ -11,6 +11,34 @@ def get_hash_and_rev ( source_list=None, hash_file_name="source_info.json" ):
     if os.path.exists (hash_file_name):
       f = open (hash_file_name, 'r')
       text = f.read ()
+      if (7*'<' in text) or (7*'=' in text) or (7*'>' in text):
+        # This has likely resulted from a merge, so check if it fits the proper pattern and remove
+        # Start by splitting the source_info file into lines using the OS linesep or '\n' as needed
+        split_by = os.linesep
+        if not split_by in text:
+            split_by = '\n'
+        lines = text.split(split_by)
+        # Use a nested list comprehension ("incomprehension"?) to find and count merge markers
+        if [ [ 7*c in l for l in lines ].count(True) for c in ['<','=','>'] ] == [1,1,1]:
+          # There is only one line for each merge marker, so find them with another list incomprehension
+          marker_line_numbers = [[lines.index (l) for l in lines if l.startswith (7 * c)] [0] for c in ['<', '=', '>']]
+          if (marker_line_numbers[0] == marker_line_numbers[1] - 2) and (marker_line_numbers[1] == marker_line_numbers[2] - 2):
+            # The marker line numbers are in the expected order and spacing, so remove all lines between them (inclusive)
+            good_lines = lines[0:marker_line_numbers[0]] + lines[marker_line_numbers[2]+1:]
+            # Replace "text" with a new version without the merge lines:
+            text = os.linesep.join ( good_lines )
+            # At this point, the text should be valid JSON without any of the merge lines.
+            # This also means it will not have the "current_hash" key in the dictionary (removed above).
+            # That's important so that subsequent code will add the "current_hash" and rewrite the file.
+            print ( "\nSource Text:\n" + str(text) + "\n\n")
+          else:
+            # The JSON file contains an unexpected ordering/spacing of merge markers (not [<,=,>]) ... so exit with an error
+            print ("Unexpected ordering or spacing of merge markers in " + str (hash_file_name))
+            exit (98)
+        else:
+          # The JSON file contains an unexpected configuration of merge markers (not [1,1,1]) ... so exit with an error
+          print ( "Unexpected configuration of merge markers in " + str(hash_file_name))
+          exit(99)
       hash_dict = json.loads (text)
 
     m = hashlib.sha1()
