8298a9c51e09728bf753d29a3e7851c67c8abe7a
Bob Kuczewski <bobkuczewski@salk.edu>
2020-04-21 12:33:44 -0700

    Got the optimized scaling option working (only loads large image once).

--- source/PySide2/multi_scale_job.py
+++ source/PySide2/multi_scale_job.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
-import time
-import random
-
-import argparse
+import sys
 import align_swiftir
 
 if __name__ == '__main__':
+  # Args: self source_image s1 t1 s2 t2 s3 t3
+  # Where s# is a scale value and t# is a target image name
+  if  not ( (2 * int(len(sys.argv) / 2)) == len(sys.argv) ):
+    print ( "Error: " + arv[0] + " requires parameters of the form: source_image [scale_value target_image]...")
+  else:
+    # Read the source image
+    source_image_file_name = sys.argv[1]
+    source_image = align_swiftir.swiftir.loadImage(source_image_file_name)
 
-  parser = argparse.ArgumentParser ()
-  parser.add_argument("scale", type=int, help="integer value to scale image")
-  parser.add_argument("infile", help="file name of the original image")
-  parser.add_argument("outfile", help="file name of the scaled image")
-  arg_space = parser.parse_args ()
+    # Build a list of scale/image pairs for each target image
+    m = sys.argv[2:]
+    scale_target_pair_list = [[m[2*i],m[(2*i)+1]] for i in range(int(len(m)/2)) ]
 
-  print ( "SCALE: " + str(arg_space.scale) + " " + arg_space.infile + " " + arg_space.outfile )
-
-  img = align_swiftir.swiftir.scaleImage (align_swiftir.swiftir.loadImage(arg_space.infile), fac=arg_space.scale)
-  align_swiftir.swiftir.saveImage (img, arg_space.outfile)
+    for arg_pair in scale_target_pair_list:
+      scaled_image = align_swiftir.swiftir.scaleImage ( source_image, fac=int(arg_pair[0]) )
+      align_swiftir.swiftir.saveImage ( scaled_image, arg_pair[1] )
