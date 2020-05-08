b9dce96b1fd5e2c45083d2bb10014148b3dc61f6
Bob Kuczewski <bobkuczewski@salk.edu>
2020-05-08 14:58:27 -0700

    Addition of single_crop_job.py (currently a copy of single_scale_job.py)

---
+++ source/PySide2/single_crop_job.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import random
+
+import argparse
+import align_swiftir
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser ()
+  parser.add_argument("scale", type=int, help="integer value to scale image")
+  parser.add_argument("infile", help="file name of the original image")
+  parser.add_argument("outfile", help="file name of the scaled image")
+  arg_space = parser.parse_args ()
+
+  print ( "SCALE: " + str(arg_space.scale) + " " + arg_space.infile + " " + arg_space.outfile )
+
+  img = align_swiftir.swiftir.scaleImage (align_swiftir.swiftir.loadImage(arg_space.infile), fac=arg_space.scale)
+  align_swiftir.swiftir.saveImage (img, arg_space.outfile)
+
+  sys.stdout.close()
+  sys.stderr.close()
