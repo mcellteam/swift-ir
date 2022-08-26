25e0cb0264fb5184e6b660ffdcb0b73269fa349f
Joel Yancey <j.y@ucla.edu>
2022-08-25 18:39:07 -0700

    Further progress in configuring setuptools

--- __init__.py
+++ __init__.py
@@ -1,12 +1,24 @@
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass  # package is not installed
+
 __all__ = []
 
 print(f'{__name__}')
+
+from . import alignEM
+from .alignEM import main
+__all__.extend(main.__all__)
 
 from . import src
 from .src import *
 __all__.extend(src.__all__)
 __all__.append('src')
 
-def hello_world():
-    print("Hello world")
+
+
+
+
 
