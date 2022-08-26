1372401f5b12d61c5f7a08180a569cb13c3e7e52
Joel Yancey <j.y@ucla.edu>
2022-08-25 18:59:53 -0700

    Tweak setup.py config again

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ setup(
     platforms=["any"],
     url="https://github.com/mcellteam/swift-ir/tree/development_ng",
     packages=find_packages(),
-    scripts=['alignEM.py'],
     python_requires=">=3.9",
     entry_points={
         "console_scripts": [
-            "download = downloader.cli:main",
+            "alignem = alignEM:main",
         ]
     }
 
