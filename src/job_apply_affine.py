92b9d2ea746479e7946a2f617f1aa21c98fef8d6
Joel Yancey <j.y@ucla.edu>
2022-08-31 18:31:18 -0700

    Move imageAffine code into job script

--- src/job_apply_affine.py
+++ src/job_apply_affine.py
@@ -8,7 +8,34 @@ import sys
 import logging
 import numpy as np
 import traceback
-from swiftir import affineImage, saveImage, loadImage
+import cv2
+# from swiftir import affineImage, saveImage, loadImage
+from swiftir import saveImage, loadImage
+
+def shiftAffine(afm, dx):
+    return afm + np.array([[0,0,dx[0]],[0,0,dx[1]]])
+
+def affineImage(afm, img, rect=None, grayBorder=False ):
+    '''AFFINEIMAGE - Apply an affine transformation to an image
+    res = AFFINEIMAGE(afm, img) returns an image the same size of IMGimage_funcs
+    looking up pixels in the original using affine transformation.
+    res = AFFINEIMAGE(afm, img, rect), where rect is an (x0,y0,w,h)-tuple
+    as from MODELBOUNDS, returns the given rectangle of model space.
+    If grayBorder is True, set the image border color to the mean image value'''
+    if grayBorder:
+      border = img.mean()
+    else:
+      border = 0
+    if rect is None:
+        return cv2.warpAffine(img, afm, (img.shape[1],img.shape[0]),
+                              flags=cv2.WARP_INVERSE_MAP + cv2.INTER_LINEAR,
+                              borderValue=border)
+    else:
+        p1 = applyAffine(afm, (0,0))
+        p2 = applyAffine(afm, (rect[0], rect[1]))
+        return cv2.warpAffine(img, shiftAffine(afm, p2-p1), (rect[2], rect[3]),
+                              flags=cv2.WARP_INVERSE_MAP + cv2.INTER_LINEAR,
+                              borderValue=border)
 
 logger = logging.getLogger(__name__)
 
