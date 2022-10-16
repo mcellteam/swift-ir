8eacce4979f56730bef198431eb2ada066de9b0f
Joel Yancey <j.y@ucla.edu>
2022-10-16 15:53:55 -0700

    tensorstore on by default

--- src/config.py
+++ src/config.py
@@ -34,8 +34,8 @@ CNAME = 'zstd'
 CLEVEL = 5
 
 '''Other Defaults'''
-# USE_TENSORSTORE = True
-USE_TENSORSTORE = False
+USE_TENSORSTORE = True
+# USE_TENSORSTORE = False
 NO_EMBED_NG = False
 TACC_MAX_CPUS = 128  # 128 hardware cores/node on Lonestar6
 
