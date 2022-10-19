079289da42c81f1d861b3c5acf37beb64c6be4b5
Joel Yancey <j.y@ucla.edu>
2022-10-19 03:07:19 -0700

    Update

--- src/ui/snr_plot.py
+++ src/ui/snr_plot.py
@@ -17,20 +17,23 @@ class SnrPlot(pg.PlotWidget):
         super().__init__(**kwargs)
 
         self.setAntialiasing(False)
-        self.setAspectLocked(True)
+        # self.setAspectLocked(True)
         self.showGrid(x=True, y=True, alpha=220)  # alpha: 0-255
-        self.getPlotItem().enableAutoRange()
+        # self.getPlotItem().enableAutoRange()
         self.hoverable = True
         self.hoverSize = 11
         self.setFocusPolicy(Qt.NoFocus)
         font = QFont(); font.setPixelSize(12)
+        font = QFont()
+        font.setPixelSize(14)
+        self.getAxis("bottom").tickFont = font
         self.getAxis("bottom").setStyle(tickFont=font)
-        self.getAxis("bottom").setHeight(28)
+        self.getAxis("bottom").setHeight(14)
         self.getAxis("left").setStyle(tickFont=font)
         self.getAxis("left").setWidth(36)
         style = {'color': '#f3f6fb;', 'font-size': '14px'}
         self.setLabel('left', 'SNR', **style)
-        self.setLabel('bottom', 'Layer #', **style)
+        # self.setLabel('bottom', 'Layer #', **style)
 
         self.setCursor(Qt.CrossCursor)
 
