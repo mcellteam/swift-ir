00f9b7ed374955c54f6442271b246bc3f0e87632
Joel Yancey <j.y@ucla.edu>
2022-11-27 21:26:25 -0800

    Clean up config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@
 
 '''This file is for initializing global config and 'data' the dictionary data state in memory.'''
 
-# __all__ = ['data', 'main_window']
 __all__ = []
 
 LOG_LEVEL = 1
@@ -10,22 +9,11 @@ LOG_LEVEL = 1
 '''Main Objects'''
 data = None
 main_window = None
-# viewports = None
 opengllogger = None
-
-selected = None # type = neuroglancer.json_wrappers.typed_string_map.<locals>.Map (s.selected_values)
-'''
-cfg.selected
-AlignEM [2]: Map({"ref_4": {"value": 145}, "base_4": {"value": 56}, "matchpoint_ref": {}, "matchpoint_base": {}, "aligned_4": {"value": 149}})
-'''
-
-# mouse_coords = None # type = numpy.nd array (s.mouse_voxel_coords)
-# matchpoint_layer = None
-# viewport_temp_state = None
+selected = None
 
 '''Default Window Size'''
 WIDTH, HEIGHT = 1180, 800
-# WIDTH, HEIGHT = 1500, 1200
 
 '''Default Alignment Params'''
 DEFAULT_SWIM_WINDOW       = float(0.8125)
@@ -50,17 +38,11 @@ CLEVEL = 5
 DUMMY = False
 USE_TENSORSTORE = True
 NO_EMBED_NG = False
-# TACC_MAX_CPUS = 128  # 128 hardware cores/node on Lonestar6
 TACC_MAX_CPUS = 124  # 128 hardware cores/node on Lonestar6
-# REFRESH_RATE = 300 # GUI refresh/synchronization rate (ms). None = OFF.
-# REFRESH_RATE = None
-# MULTIVIEW = True
 SUPPORT_NONSQUARE = True
 DEBUG_MP = False
 NO_SPLASH = True
 USE_PYTHON = False
-# USE_TORNADO = False
-# USE_NG_WEBDRIVER = True
 USE_TORNADO = True
 USE_NG_WEBDRIVER = False
 SHADER = None
@@ -68,22 +50,8 @@ THEME = 0
 SHOW_UI_CONTROLS = True
 MP_LINEWEIGHT = 3
 MP_SIZE = 5
-
-
 USE_FILE_IO = 0
 CODE_MODE = 'c'
 HTTP_PORT = 9000
-
 MATCH_POINT_MODE = False
-
-# cfg.ICON_COLOR = '#d3dae3'
-# cfg.ICON_COLOR = '#7c7c7c'
-# ICON_COLOR = '#455364' # off blue-ish color
-# ICON_COLOR = '#F3F6FB' # snow white
-# ICON_COLOR = '#DAEBFE' # lightest ucla blue
-# ICON_COLOR = '#003B5C'
 ICON_COLOR = '#2774AE'
-# ICON_COLOR = '#455364'
-
-
-
