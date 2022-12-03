08f00bcdc41a0a99611f832a226dbb3433665bf6
Joel Yancey <j.y@ucla.edu>
2022-12-03 04:49:19 -0800

    refactor

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1081,11 +1081,10 @@ class MainWindow(QMainWindow):
         logger.debug('Changing To Scale %s (caller %s)...' % (s, inspect.stack()[1].function))
         # self.initNgServer(scales=[s])
         # self.refreshNeuroglancerURL(s=cfg.data.scale())
-        # if cfg.SIMULTANEOUS_SERVERS:
-        #     self.initNgViewer(scales=[cfg.data.scale()])
-        # else:
-        #     self.initNgServer(scales=[cfg.data.scale()])
-        self.initNgServer(scales=[cfg.data.scale()])
+        if cfg.SIMULTANEOUS_SERVERS:
+            self.initNgViewer(scales=[cfg.data.scale()])
+        else:
+            self.initNgServer(scales=[cfg.data.scale()])
         self.jump_to(cfg.data.layer())
         self.read_project_data_update_gui()
         self.updateHistoryListWidget(s=s)
