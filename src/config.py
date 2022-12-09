4b8ff9ec024d530f220f1d131bbd39558796ed49
Joel Yancey <j.y@ucla.edu>
2022-12-08 21:23:26 -0800

    refactor

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ data = None
 main_window = None
 opengllogger = None
 selected = None
+ng_worker = None
 
 '''Default Window Size'''
 # WIDTH, HEIGHT = 1180, 780
@@ -29,8 +30,8 @@ DEFAULT_INITIAL_SCALE     = float(1.0000)
 DEFAULT_RESX, DEFAULT_RESY, DEFAULT_RESZ = 2, 2, 50
 
 '''Default Zarr Chunk Shape'''
-# CHUNK_X, CHUNK_Y, CHUNK_Z = 512, 512, 1
-CHUNK_X, CHUNK_Y, CHUNK_Z = 64, 64, 64
+CHUNK_X, CHUNK_Y, CHUNK_Z = 512, 512, 1
+# CHUNK_X, CHUNK_Y, CHUNK_Z = 64, 64, 64
 
 '''Default Compression Parameters'''
 CNAME = 'zstd'
