6cba43db12792acdcb1d423fb50bd690ee0c3a8b
Joel Yancey <j.y@ucla.edu>
2022-12-08 21:23:26 -0800

    refactor

--- src/helpers.py
+++ src/helpers.py
@@ -172,23 +172,23 @@ def get_snr_average(scale) -> float:
 
 
 def get_tensor_handle_unal():
-    return cfg.main_window.ng_worker.unal_dataset
+    return cfg.ng_worker.unal_dataset
     # return cfg.main_window.ng_workers[cfg.data.scale()].unal_dataset
 
 def get_tensor_handle_al():
-    return cfg.main_window.ng_worker.al_dataset
+    return cfg.ng_worker.al_dataset
     # return cfg.main_window.ng_workers[cfg.data.scale()].al_dataset
 
 def get_refLV():
-    return cfg.main_window.ng_worker.refLV
+    return cfg.ng_worker.refLV
     # return cfg.main_window.ng_workers[cfg.data.scale()].refLV
 
 def get_baseLV():
-    return cfg.main_window.ng_worker.baseLV
+    return cfg.ng_worker.baseLV
     # return cfg.main_window.ng_workers[cfg.data.scale()].baseLV
 
 def get_alLV():
-    return cfg.main_window.ng_worker.baseLV
+    return cfg.ng_worker.baseLV
     # return cfg.main_window.ng_workers[cfg.data.scale()].baseLV
 
 def renew_directory(directory:str) -> None:
