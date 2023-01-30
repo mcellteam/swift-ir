60804abe2666dee065410197c61c9e538e6b4d7b
Joel Yancey <j.y@ucla.edu>
2023-01-30 13:54:30 -0800

    refactor

--- src/ui/tab_project.py
+++ src/ui/tab_project.py
@@ -137,11 +137,11 @@ class ProjectTab(QWidget):
         #                              print(f'QWebengineView Render Process Terminated!'
         #                                    f' terminationStatus:{terminationStatus}'))
 
-        # self.ng_browser.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
-        # self.ng_browser.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
-        # self.ng_browser.settings().setAttribute(QWebEngineSettings.AllowRunningInsecureContent, True)
-        # self.ng_browser.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessFileUrls, True)
-        # self.ng_browser.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, True)
+        self.ng_browser.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        self.ng_browser.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        self.ng_browser.settings().setAttribute(QWebEngineSettings.AllowRunningInsecureContent, True)
+        self.ng_browser.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessFileUrls, True)
+        self.ng_browser.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, True)
 
         self.ng_browser_container = QWidget()
         self.ng_browser_container.setObjectName('ng_browser_container')
