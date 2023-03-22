7d932f78fdc7513e8d7ff8e181394a23d059723d
Joel Yancey <j.y@ucla.edu>
2023-03-22 00:12:46 -0700

    refactor

---
+++ src/ui/timer.py
@@ -0,0 +1,45 @@
+'''source: https://realpython.com/python-timer/'''
+
+import time
+import logging
+logger = logging.getLogger(__name__)
+
+
+class TimerError(Exception):
+
+    """A custom exception used to report errors in use of Timer class"""
+
+
+class Timer:
+
+    def __init__(self):
+
+        self._start_time = None
+
+
+    def start(self):
+
+        """Start a new timer"""
+
+        if self._start_time is not None:
+
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+
+
+        self._start_time = time.perf_counter()
+
+
+    def stop(self):
+
+        """Stop the timer, and report the elapsed time"""
+
+        if self._start_time is None:
+
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+
+
+        elapsed_time = time.perf_counter() - self._start_time
+
+        self._start_time = None
+
+        logger.critical(f"Elapsed time: {elapsed_time:0.4f} seconds")
