b2aa7442d6e24553d3fa486421eb1ff54edc8dc5
Joel Yancey <j.y@ucla.edu>
2023-04-26 18:57:15 -0700

    refactor

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 from setuptools import setup, find_packages
+from distutils.extension import Extension
 # setup(use_scm_version=True)
 
 with open("README.md", "r") as fh:
@@ -7,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="alignem",
-    version="0.0.1",
+    version="0.5.4.11",
     author="Joel Yancey,",
     author_email="joelgyancey@ucla.edu",
     description="AlignEM-SWIFT is a graphical tool for aligning serial section electron micrographs using SWiFT-IR.",
@@ -16,14 +17,26 @@ setup(
     platforms=["any"],
     url="https://github.com/mcellteam/swift-ir/tree/development_ng",
     # packages=find_packages(),
-    packages=setuptools.find_packages(where=".", exclude=("./tests","/.docs")),
+    # packages=setuptools.find_packages(where=".", exclude=("./tests","/.docs")),
+    # package_dir={'alignem':''},
+    packages=['src','src.lib','src.lib.bin_darwin','src.lib.bin_linux','src.lib.bin_tacc'
+              'src.resources','src.style','src.ui','src.ui.models','src.utils'],
+    ext_package='src.lib',
+
+    ext_modules=[Extension('iavg', ['iavg.c']),
+                 Extension('iscale', ['iscale.c']),
+                 Extension('iscale2', ['iscale2.c']),
+                 Extension('mir', ['mir.c']),
+                 Extension('remod', ['remod.c']),
+                 Extension('swim', ['swim.c']),
+                 ],
     python_requires=">=3.9",
+    scripts=['alignem.py'],
     entry_points={
         "console_scripts": [
             "alignem = alignem:main",
         ]
     }
-
 )
 
 if __name__=="__main__":
