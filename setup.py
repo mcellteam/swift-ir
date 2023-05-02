9e6dd567693b048925e8e7220216ef50f68998a7
Joel Yancey <j.y@ucla.edu>
2023-05-02 11:01:56 -0700

    refactor

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="alignem",
-    version="0.5.438",
+    version="0.5.440",
     author="Joel Yancey,",
     author_email="joelgyancey@ucla.edu",
     description="AlignEM-SWIFT is a graphical tool for registering serial section electron micrographs using SWiFT-IR.",
