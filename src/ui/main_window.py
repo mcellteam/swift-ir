b07f5124367a1709c6ae833ba4f9b99df9f0dfe5
Joel Yancey <j.y@ucla.edu>
2023-05-05 17:02:27 -0700

    Critical patch, refactor

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -318,9 +318,9 @@ class MainWindow(QMainWindow):
         self._scales_combobox_switch = 0 #1125
         self._isPlayingBack = 0
         self._isProfiling = 0
-        logger.info('WebPage() >>>>')
+        # logger.info('WebPage() >>>>')
         self.detachedNg = WebPage()
-        logger.info('<<<< WebPage()')
+        # logger.info('<<<< WebPage()')
         self._lastRefresh = 0
         # self._corrSpotDrawerSize = 140
         self._corrSpotDrawerSize = 160
