12826b60ef14a6c06d863070834ee2d3f367012b
Joel Yancey <j.y@ucla.edu>
2023-06-06 15:29:09 -0500

    refactor

--- src/ui/dialogs.py
+++ src/ui/dialogs.py
@@ -135,7 +135,40 @@ class SaveExitAppDialog(QDialog):
         self.layout.addWidget(self.buttonBox, alignment=Qt.AlignCenter)
         self.setLayout(self.layout)
 
+def FileDialog(directory='', forOpen=True, fmt='', isFolder=False):
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    options |= QFileDialog.DontUseCustomDirectoryIcons
+    dialog = QFileDialog()
+    dialog.setOptions(options)
 
+    dialog.setFilter(dialog.filter() | QDir.Hidden)
+
+    # ARE WE TALKING ABOUT FILES OR FOLDERS
+    if isFolder:
+        dialog.setFileMode(QFileDialog.DirectoryOnly)
+    else:
+        dialog.setFileMode(QFileDialog.AnyFile)
+    # OPENING OR SAVING
+    dialog.setAcceptMode(QFileDialog.AcceptOpen) if forOpen else dialog.setAcceptMode(QFileDialog.AcceptSave)
+
+    # SET FORMAT, IF SPECIFIED
+    if fmt != '' and isFolder is False:
+        dialog.setDefaultSuffix(fmt)
+        dialog.setNameFilters([f'{fmt} (*.{fmt})'])
+
+    # SET THE STARTING DIRECTORY
+    if directory != '':
+        dialog.setDirectory(str(directory))
+    else:
+        dialog.setDirectory(str(os.getenv('SCRATCH')))
+
+
+    if dialog.exec_() == QDialog.Accepted:
+        path = dialog.selectedFiles()[0]  # returns a list
+        return path
+    else:
+        return ''
 
 class QFileDialogPreview(QFileDialog):
     def __init__(self, *args, **kwargs):
@@ -168,6 +201,8 @@ class QFileDialogPreview(QFileDialog):
         self.layout().addLayout(self.extra_layout, 3, 3, 1, 1)
         self.currentChanged.connect(self.onChange)
         self.fileSelected.connect(self.onFileSelected)
+        self.directoryEntered.connect(lambda: print('directory entered'))
+        self.directoryUrlEntered.connect(lambda: print('directory URL entered'))
         self.filesSelected.connect(self.onFilesSelected)
         self._fileSelected = None
         self._filesSelected = None
