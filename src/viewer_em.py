ea8672b38c3dd0c828b12bd1300fd22de4b2647c
Joel Yancey <j.y@ucla.edu>
2023-06-13 14:54:34 -0500

    refactor

--- src/viewer_em.py
+++ src/viewer_em.py
@@ -514,12 +514,13 @@ class EMViewer(AbstractEMViewer):
 
         w = cfg.project_tab.webengine.width()
         # h = max(cfg.main_window.globTabs.height() - 20, 520)
+        h = cfg.main_window.globTabs.height()
         if getData('state,show_ng_controls'):
             extra_space = 54
         else:
             extra_space = 26
 
-        h = max(cfg.project_tab.w_ng_display.height() - extra_space, 420 - extra_space)
+        # h = max(cfg.project_tab.w_ng_display.height() - extra_space, 420 - extra_space) #Prev
         # h = cfg.main_window.globTabs.height() - 20
         self.initZoom(w=w, h=h, adjust=1.18)
 
