ce29e79c8ddfdd3f59c66b99eb2147094cc5a5cb
Joel Yancey <j.y@ucla.edu>
2023-07-20 04:23:33 -0700

    refactor

--- src/recipe_maker.py
+++ src/recipe_maker.py
@@ -507,8 +507,7 @@ class align_ingredient:
 
 
         for name in ms_names:
-            self.crop_str_mir = f"""B {w} {h} 1\nZ\nF {name}\n0 0 {x1} {y1}\n{w} 0 {x2} {y1}\n
-            {w} {h} {x2} {y2}\n0 {h} {x1} {y2}\nT\n0 1 2\n2 3 0\nW {name}"""
+            self.crop_str_mir = f"""B {w} {h} 1\nZ\nF {name}\n0 0 {x1} {y1}\n{w} 0 {x2} {y1}\n{w} {h} {x2} {y2}\n0 {h} {x1} {y2}\nT\n0 1 2\n2 3 0\nW {name}"""
             try:
                 o = run_command(self.recipe.mir_c,
                                 arg_list=[],
