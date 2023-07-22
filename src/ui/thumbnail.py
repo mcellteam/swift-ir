81ef09cb7b1fca76a34ea6cd7cf7f41941072183
Joel Yancey <j.y@ucla.edu>
2023-07-21 21:29:57 -0700

    refactor add -1

--- src/ui/thumbnail.py
+++ src/ui/thumbnail.py
@@ -248,7 +248,7 @@ class ThumbnailFast(QLabel):
                     try:
                         self.pixmap().fill(QColor('#dadada'))
                         font = QFont()
-                        size = max(min(int(11 * (max(pm.height(), 1) / 80)), 16), 5)
+                        size = max(min(int(11 * (max(pm.height(), 1) / 80)), 14), 5)
                         font.setPointSize(size)
                         # font.setBold(True)
                         qp.setFont(font)
