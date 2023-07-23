57c66e2183691cb371270ccb161525e3184be980
Joel Yancey <j.y@ucla.edu>
2023-07-22 20:17:20 -0700

    set iters back to 1 for refinement

--- src/recipe_maker.py
+++ src/recipe_maker.py
@@ -72,6 +72,8 @@ class align_recipe:
         else:
             self.wht = self.alignment['swim_settings']['signal-whitening']
             self.iters = self.alignment['swim_settings']['iterations']
+        if self.meta['isRefinement']:
+            self.iters = 1
         self.grid_custom_regions  = self.alignment['swim_settings']['grid-custom-regions']
         self.ingredients = []
         self.initial_rotation = float(self.defaults['initial-rotation'])
