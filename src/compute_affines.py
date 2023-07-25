eafd598a178a8fe692058f454470e59f4a81815f
Joel Yancey <j.y@ucla.edu>
2023-07-24 19:11:10 -0700

    refactor

--- src/compute_affines.py
+++ src/compute_affines.py
@@ -208,7 +208,8 @@ def ComputeAffines(scale, path, start=0, end=None, use_gui=True, renew_od=False,
 
         # PRETTY SURE THIS IS THE BEST/FASTEST/LEAST MEMORY CONSUMPTION/REPORTS ERRORS BACK SOONEST
         # with ThreadPool(processes=cpus) as pool:
-        with mp.Pool(processes=cpus, maxtasksperchild=1) as pool:
+        ctx = mp.get_context('forkserver')
+        with ctx.Pool(processes=cpus, maxtasksperchild=1) as pool:
             results = [pool.apply_async(func=run_recipe, args=(task,), callback=update_pbar) for task in tasks]
             pool.close()
             all_results = [p.get() for p in results]
