eeaa84ee0adc13345fd4f7b059aeded3cf19b13e
Joel Yancey <j.y@ucla.edu>
2023-07-31 15:21:38 -0700

    refactor

--- src/compute_affines.py
+++ src/compute_affines.py
@@ -193,7 +193,7 @@ def ComputeAffines(scale, path, start=0, end=None, use_gui=True, renew_od=False,
         ctx = mp.get_context('forkserver')
         # with ctx.Pool(processes=cpus, maxtasksperchild=1) as pool:
         # with mp.Pool(processes=cpus, maxtasksperchild=1) as pool:
-        with ThreadPoolExecutor(max_workers=40) as pool:
+        with ThreadPoolExecutor(max_workers=30) as pool:
             # with ctx.Pool() as pool:
 
 
