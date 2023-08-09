7ba56ecd2755211ce603cf2fc3548af5bad4f4dd
Joel Yancey <j.y@ucla.edu>
2023-08-08 22:15:41 -0700

    reduce default window size

--- src/config.py
+++ src/config.py
@@ -78,7 +78,7 @@ is_mendenhall = False
 # WIDTH, HEIGHT = 1380, 900
 # WIDTH, HEIGHT = 1180, 720
 # WIDTH, HEIGHT = 1320, 740
-WIDTH, HEIGHT = 1400, 800
+WIDTH, HEIGHT = 1200, 700
 
 '''Default Alignment Params'''
 
