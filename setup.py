2908eb2eb837b9d671bba41503218be07dfcf54c
Joel Yancey <j.y@ucla.edu>
2023-11-21 00:01:31 -0800

    Update version number to v0.5.51

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="alignem",
-    version="0.5.50",
+    version="0.5.51",
     author="Joel Yancey,",
     author_email="joelgyancey@ucla.edu",
     description="alignEM is a graphical tool for registering serial section electron micrographs using SWiFT-IR.",
